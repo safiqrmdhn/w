1170a1f47a3127a454cb949233fe1eaea5ff2bcc
bowouhuy <wibowo55761@gmail.com>
2020-12-14 02:19:44 +0700

    fix

--- index.php
+++ index.php
@@ -4,8 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hitung Weton</title>
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-    <link rel="icon" href="favicon.ico" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
     <link rel="preconnect" href="https://fonts.gstatic.com">
@@ -38,7 +36,7 @@
                     <label for="nama" class="col-sm-2 col-form-label">Nama Pasangan</label>
                     <div class="col-sm-10">
                         <input type="text" class="form-control" id="inputNama2">
-                        <small id="nama-alert" class="form-text text-danger"></small>
+                        <small id="nama2-alert" class="form-text text-danger"></small>
                     </div>
                 </div>
                 <div class="mb-1 row">
