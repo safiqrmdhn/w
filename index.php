90d73b1da436956abfd31a9010adb06117e069f4
bowouhuy <wibowo55761@gmail.com>
2020-12-13 14:30:06 +0700

    push ke heroku

--- index.php
+++ index.php
@@ -3,29 +3,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <title>Hitung Weton</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+    crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@491&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
-<div class="container-md mt-5 col-10">
+<div class="container-md mt-5 col-11">
     <h1>Hitung</h1>
-    <div class="card">
+    <img src="head-min.jpg" alt="" srcset="" class="ikon">
+    <div class="card mb-3">
         <div class="card-body" >
             <form>
-                <div class="mb-3 row">
-                    <label for="nama" class="col-sm-2 col-form-label">Nama</label>
+                <div class="mb-1 row">
+                    <label for="nama" class="col-sm-2 col-form-label">Nama Kamu</label>
                     <div class="col-sm-10">
                     <input type="text" class="form-control" id="inputNama">
                     </div>
                 </div>
-                <div class="mb-3 row">
-                    <label for="inputNama" class="col-sm-2 col-form-label">Tanggal</label>
+                <div class="mb-1 row">
+                    <label for="inputNama" class="col-sm-2 col-form-label">Tanggal Lahir</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="inputTanggal">
+                        <input type="date" class="form-control" id="inputTanggal">
                     </div>
                 </div>
-                <input type="button" value="Submit" id="submit" class="btn btn-primary" >
+                <div class="mb-1 row">
+                    <label for="nama" class="col-sm-2 col-form-label">Nama Pasangan</label>
+                    <div class="col-sm-10">
+                    <input type="text" class="form-control" id="inputNama">
+                    </div>
+                </div>
+                <div class="mb-1 row">
+                    <label for="inputNama" class="col-sm-2 col-form-label">Tanggal Lahir</label>
+                    <div class="col-sm-10">
+                        <input type="date" class="form-control" id="inputTanggal">
+                    </div>
+                </div>
+                <input type="button" value="Hitung" id="submit" class="btn btn-primary mt-2 col-12 btn-hitung" >
             </form>
         </div>
     </div>
--- script.js
+++ script.js
@@ -6,16 +6,15 @@ $('#submit').on('click', function () {
     var tanggal = $('#inputTanggal').val();
     var explode =tanggal.split("-");
 
-    // Menampilkan bulan
+    // Menampilkan tanggal, bulan, tahun
     var bulan = ["", "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
     
     if (explode[1] == "10" || explode[1] == "11" || explode[1] == "12"){
         
         var bln=explode[1];
     }else {
-        var bln=explode[1].substr(1,1);
+        bln=explode[1].substr(1,1);
     }
-    console.log(bln);
 
     // Menentukan Hari Masehi
 
@@ -28,39 +27,39 @@ $('#submit').on('click', function () {
 		a = ['',31,28,31,30,31,30,31,31,30,31,30,31]
     }
 
-	var w = explode[1];
+    var siklus = Math.floor((explode[0]-1)/4);
+
+    var sisaSiklus = (explode[0]-1) % 4;
     
-    var q = 0;
-        for(var i=1; i<w; i++){
-            q = q+a[i];
+     if (sisaSiklus !== 0 ){
+         sisaSiklus = sisaSiklus * 365;
         }
+    var hariSiklus = siklus * 1461;
 
-        var zzz=10/4;
-        typeof zzz;
-        console.log(zzz);
-    // if(explode[0]%4 == 0){
-    //     var q = 366
-    // }else{
-    //     var q = 365
-    // }
+        var w = explode[1];
+        
+        var q = 0;
+            for(var i=1; i<w; i++){
+                q = q+a[i];
+            }
+        var haridalamTahun = q + parseInt(explode[2]);
 
-    var b = q+parseInt(explode[2]);
+    var jumlahHari = hariSiklus + sisaSiklus + haridalamTahun;
+
+    //Koreksi Gregorius dan selisih hijriyah dan masehi
+
+    var selisihdankoreksi = 227029;
+    jumlahHari = jumlahHari - selisihdankoreksi;
+  
+    var day = ["Kamis", "Jum'at","Sabtu","Minggu","Senin","Selasa","Rabu","Kamis"]
+    var pasaran = ["Kliwon","Legi","Pahing","Pon","Wage","Kliwon"]
     
-	var c = (explode[0]-1)/4;
+    // Masehi
+    var xmasehi = jumlahHari % 7;
 
-	var d = (explode[0]+b+c)%7; // masehi
-    var xmasehi = Math.round(d)
-    var e = (c+b)%5; // pasaran
-    var xpasaran = Math.round(e)
-    
-    var day = ["Jum'at","Sabtu","Minggu","Senin","Selasa","Rabu","Kamis"]
-    var pasaran = ["Legi","Pahing","Pon","Wage","Kliwon"]
-
-    console.log(pasaran[xpasaran])
-    console.log(day[xmasehi])
-    console.log(d)
-    console.log(e)
-
+    // Pasaran
+    var xpasaran = jumlahHari % 5;
+    console.log(pasaran[xpasaran]);
 
     $("#result").append(`                    
     <div class="card mt-2">
